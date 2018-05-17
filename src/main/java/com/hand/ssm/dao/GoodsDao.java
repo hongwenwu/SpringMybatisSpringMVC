6d6fd99b99132e6e149b9e2d5e21ef483794f5e5
CodeForSanil <CodeForSanil@users.noreply.github.com>
2018-05-17 15:48:24 +0800

    修改dao
    修改dao

--- src/main/java/com/hand/ssm/dao/GoodsDao.java
+++ src/main/java/com/hand/ssm/dao/GoodsDao.java
@@ -2,8 +2,16 @@ package com.hand.ssm.dao;
 
 import com.hand.ssm.dto.Goods;
 
+/**
+ * @author wangz
+ */
 public interface GoodsDao {
 
+	/**
+	 * 查询产品信息
+	 * @param goodId 产品id
+	 * @return Goods
+	 */
 	Goods getGoodByPrimaryKey(long goodId);
 	
 }
