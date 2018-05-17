e985c75bf3cce9357909c23cf42d616194af9b2a
CodeForSanil <CodeForSanil@users.noreply.github.com>
2018-05-17 11:54:32 +0800

    Revert "修改控制器"
    This reverts commit 4e72b0264d0e06e44787a5d2a3395a6cb896f789.

--- src/main/java/com/hand/ssm/controller/GoodsController.java
+++ src/main/java/com/hand/ssm/controller/GoodsController.java
@@ -3,7 +3,6 @@ package com.hand.ssm.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.hand.ssm.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +15,13 @@ import com.hand.ssm.dto.Goods;
 public class GoodsController {
 	
 	@Autowired
-	GoodsService goodsService;
+	GoodsDao goodsDao;
 	
 	@RequestMapping("/good")
 	public String getGood(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response){
 		long id = Long.valueOf(request.getParameter("id"));
-		Goods goods = goodsService.getGood(id);
+		
+		Goods goods = goodsDao.getGoodByPrimaryKey(id);
 		modelAndView.addObject("good", goods);
 		return "success";
 	}
