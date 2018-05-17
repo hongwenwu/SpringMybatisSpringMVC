4e72b0264d0e06e44787a5d2a3395a6cb896f789
CodeForSanil <CodeForSanil@users.noreply.github.com>
2018-05-17 11:39:58 +0800

    修改控制器
    修改控制器

--- src/main/java/com/hand/ssm/controller/GoodsController.java
+++ src/main/java/com/hand/ssm/controller/GoodsController.java
@@ -3,6 +3,7 @@ package com.hand.ssm.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.hand.ssm.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +16,12 @@ import com.hand.ssm.dto.Goods;
 public class GoodsController {
 	
 	@Autowired
-	GoodsDao goodsDao;
+	GoodsService goodsService;
 	
 	@RequestMapping("/good")
 	public String getGood(ModelAndView modelAndView, HttpServletRequest request, HttpServletResponse response){
 		long id = Long.valueOf(request.getParameter("id"));
-		
-		Goods goods = goodsDao.getGoodByPrimaryKey(id);
+		Goods goods = goodsService.getGood(id);
 		modelAndView.addObject("good", goods);
 		return "success";
 	}
